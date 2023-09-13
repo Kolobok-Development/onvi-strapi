78e528ec4adc81a701443ca12785835661203b4d
Nikita Savitskiy <s6475515243@gmail.com>
2023-09-12 22:05:47 -0400

    [fix] refactoring

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,3 +1,6 @@
+const BUCKET = process.env.YANDEX_CLOUD_BUCKET;
+const BUCKET_URL = `https://${BUCKET}.storage.yandexcloud.net`;
+
 module.exports = [
   'strapi::errors',
   'strapi::security',
@@ -9,4 +12,18 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', BUCKET_URL],
+          'media-src': ["'self'", 'data:', 'blob:', BUCKET_URL],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
---
+++ config/plugins.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-yandex-cloud',
+      providerOptions: {
+        endpoint: 'https://storage.yandexcloud.net',
+        accessKeyId: env('YANDEX_CLOUD_ACCESS_KEY_ID'),
+        secretAccessKey: env('YANDEX_CLOUD_ACCESS_SECRET'),
+        region: env('YANDEX_CLOUD_REGION'),
+        params: {
+          Bucket: env('YANDEX_CLOUD_BUCKET'),
+          CacheControl: "public, max-age=864000"
+        },
+      },
+    },
+  },
+});
